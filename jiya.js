67cad972e53866c49932ff8960427f2c996eb025
InshalFatima <38943108+InshalFatima@users.noreply.github.com>
2018-07-19 15:33:12 +0500

    Add files via upload

---
+++ jiya.js
@@ -0,0 +1,7 @@
+function check(){
+	let number1= parseInt(document.querySelecter("#abc").value);
+	let number2= parseInt(document.querySelecter("#efg").value);
+	let square = number1+number2;
+	alert(square);
+
+}
